cc8435683079d3560068ccb7a9589504854bb564
JBritoAvilez <98851943+JBritoAvilez@users.noreply.github.com>
2022-04-13 20:33:41 -0500

    Update Taller3.java

--- src/taller3/Taller3.java
+++ src/taller3/Taller3.java
@@ -5,6 +5,7 @@
  */
 package taller3;
 
+import java.util.Scanner;
 /**
  *
  * @author Laura
@@ -15,6 +16,7 @@ public class Taller3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
         SongCollection collection = new SongCollection();
         collection.AddSong("love", 1, 2019, (float) 3.5, "pop", "cancion");
         collection.AddSong("bad", 2, 2016, (float) 4.5, "pop", "cancion del año");
@@ -22,8 +24,16 @@ public class Taller3 {
         collection.AddSong("make you feel", 4, 2013, (float) 4.5, "rock", "cancion del año");
        // collection.ViewSong();
        // collection.ViewGenre();
-          collection.SearchGenre("pop");
-    
+       
+       System.out.println("De cual genero deseas ver"+"\npop"+"\nrock");
+       String genre = console.next();
+       System.out.println("Las canciones filtradas por el genero "+genre+" son:");
+       collection.SearchGenre(genre);
+             
+       System.out.println("\n\nDe cual año deseas ver"+"\n2019"+"\n2016"+"\n2015"+"\n2013");
+       int date = console.nextInt();
+       System.out.println("Las canciones filtradas por el año "+date+" son:");
+       collection.Year(date);
     }
     
 }
