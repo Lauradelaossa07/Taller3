4324def82831d06428044f908d0468e014774979
JBritoAvilez <98851943+JBritoAvilez@users.noreply.github.com>
2022-04-15 23:41:50 -0500

    Update Taller3.java
    Creacion del metodo menu

--- src/taller3/Taller3.java
+++ src/taller3/Taller3.java
@@ -6,19 +6,30 @@
 package taller3;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 /**
  *
  * @author Laura
  */
 public class Taller3 {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static int menu(){
+    int option = 0;
+    do{
+        option= Integer.parseInt(JOptionPane.showInputDialog("Que deseas hacer con las canciones a continuacion:\n\n"
+                                                             +"1 Filtrar por genero\n"
+                                                             +"2 Filtrar por año\n"
+                                                             +"3 Mostrar las lista de canciones\n"
+                                                             +"4 Salir"
+                                                             +"\n Seleccione una opción del 1 al 4"));
+    }while(option <= 0 || option >4);
+    return option;
+    }
+    
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         Scanner opc = new Scanner(System.in);
-        int opcion;
+        int option;
         
         
         SongCollection collection = new SongCollection();
@@ -42,33 +53,36 @@ public class Taller3 {
        
           do
         {
-            System.out.println("choose one of the options: ");
-            System.out.println("\n");
-            System.out.println("1. Escriba el genero que desea consultar"+"\npop"+"\nrock"+"\nJazz");
-            System.out.println("\n");
-            System.out.println("2. Filtar por año, ingresa el año: 2013,2014,2015,2016,2017,2018");
-            System.out.println("3.Mostar lista de canciones: ");
-            System.out.println("4.Exit ");
-            opcion = opc.nextInt();
+            option =menu();
+//            System.out.println("choose one of the options: ");
+//            System.out.println("\n");
+//            System.out.println("1. Escriba el genero que desea consultar"+"\npop"+"\nrock"+"\nJazz");
+//            System.out.println("\n");
+//            System.out.println("2. Filtar por año, ingresa el año: 2013,2014,2015,2016,2017,2018");
+//            System.out.println("3.Mostar lista de canciones: ");
+//            System.out.println("4.Exit ");
             
-            switch (opcion){
+            switch (option){
                 case 1:
-                     String genre = console.next();
-                     System.out.println("Las canciones filtradas por el genero "+genre+" son:");
-                     collection.SearchGenre(genre);
+                    System.out.println("1. Escriba el genero que desea consultar"+"\npop"+"\nrock"+"\nJazz");
+                    String genre = console.next();
+                    System.out.println("Las canciones filtradas por el genero "+genre+" son:");
+                    collection.SearchGenre(genre);
                     break;
                 case 2:
-                     int date = console.nextInt();
-                     System.out.println("Las canciones filtradas por el año "+date+" son:");
+                    System.out.println("2. Filtar por año, ingresa el año: 2013,2014,2015,2016,2017,2018");
+                    int date = console.nextInt();
+                    System.out.println("Las canciones filtradas por el año "+date+" son:");
                     collection.Year(date);
                     break;
                 case 3:
-                     System.out.println(" SONGS ");
-                     collection.ViewSong();
+                    System.out.println("3.Mostar lista de canciones: ");
+                    System.out.println(" SONGS ");
+                    collection.ViewSong();
                     break;
             }
         }
-        while(opcion !=4);
+        while(option !=4);
 
     }
     
